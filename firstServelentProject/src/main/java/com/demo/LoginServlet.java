fa485c4b6555fe6c8919bf6c2453d4de45ca8fbb
Rahuljn2004 <rahul.jaincs21@gmail.com>
2025-03-01 11:01:18 +0530

    UC3 Done

--- firstServelentProject/src/main/java/com/demo/LoginServlet.java
+++ firstServelentProject/src/main/java/com/demo/LoginServlet.java
@@ -9,12 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
         urlPatterns = { "/LoginServlet" },
         initParams = {
-                @WebInitParam(name = "user", value = "arunodaya"),
+                @WebInitParam(name = "user", value = "Arunodaya"),
                 @WebInitParam(name = "password", value = "jivi@11AJ"),
         }
 )
@@ -25,15 +27,34 @@ public class LoginServlet extends HttpServlet {
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
+        // Ensures the username starts with an uppercase letter
+        String capsRegex = "^[A-Z].*";
+        Pattern nameCapsPattern = Pattern.compile(capsRegex);
+        Matcher nameCapsMatcher = nameCapsPattern.matcher(user);
+
+        // Ensures the username is 3 character length
+        String nameLengthRegex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern nameLengthPattern = Pattern.compile(nameLengthRegex);
+        Matcher nameLengthMatcher = nameLengthPattern.matcher(user);
+
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userId.equals(user) && password.equals(pwd)) {
+        if(!nameCapsMatcher.matches() || !nameLengthMatcher.matches()) {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+
+            if (!nameCapsMatcher.matches())
+                out.println("<font color=red>Username must starts with Caps</font>");
+            if (!nameLengthMatcher.matches())
+                out.println("<font color=red>Username must has atleast 3 characters long</font>");
+            rd.include(request, response);
+        } else if(userId.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is wrong</font");
+            out.println("<font color=red>Either username or password is wrong</font>");
             rd.include(request, response);
         }
     }
